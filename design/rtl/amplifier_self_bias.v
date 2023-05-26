8cb315f3d6aae59b4edbf84553dde85316397302
Li-Yueting <lyt1314@stanford.edu>
2023-05-25 23:45:16 -0700

    debug flow

--- design/rtl/amplifier_self_bias.v
+++ design/rtl/amplifier_self_bias.v
@@ -2,7 +2,7 @@ module amplifier_self_bias (
     inout a,
     inout b,
     inout out,
-    inout GND,
+    inout VSS,
     inout VDD
  );
     wire n1, n2, n3;
@@ -28,12 +28,12 @@ module amplifier_self_bias (
     );
     sky130_asc_nfet_01v8_lvt_1 M7 (
         .GATE(n3),
-        .SOURCE(GND),
+        .SOURCE(VSS),
         .DRAIN(n2)
     );
     sky130_asc_nfet_01v8_lvt_1 M6 (
         .GATE(n3),
-        .SOURCE(GND),
+        .SOURCE(VSS),
         .DRAIN(n3)
     );
     sky130_asc_pfet_01v8_lvt_12 M5 (
